745344ba2408520f394e5b9dd612e55300738940
GraceYu <librayuyueran@gmail.com>
2021-10-31 16:03:56 -0500

    add tag function

--- src/components/Money/TagSection/TagSection.tsx
+++ src/components/Money/TagSection/TagSection.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Wrapper} from './TagSection.style';
 
+interface MyObject {
+	id: number,
+	name: string
+}
+
 export const TagsSection: React.FC = () => {
+	const [tags, setTags] = useState<MyObject[]>([
+		{id: 1, name: 'eret'},
+		{id: 2, name: 'ereq'},
+	])
+
+	// const [selectedTags, setSelectedTags] = useState<MyObject[]>([])
+
+	const addTags = () => {
+		const tagName = window.prompt('Please New Tag Name: ')
+		if (tagName !== null && tagName !== '') {
+			setTags([...tags, {id: 4, name: tagName}])
+		}
+	}
 
 	return (
 		<Wrapper>
-			<button>New Tag</button>
+			<button onClick={addTags}>New Tag</button>
 			<ol>
-				<li>Residence</li>
-				<li>Clothing</li>
-				<li>Food</li>
-				<li>Travel</li>
+				{tags.map(tag =>
+					<li key={tag.id}>{tag.name}</li>
+				)}
 			</ol>
 		</Wrapper>
 	)
