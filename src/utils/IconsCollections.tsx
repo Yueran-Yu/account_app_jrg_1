4d464c87f31702fcd8f4c61509f5fe34953790e5
GraceYu <librayuyueran@gmail.com>
2021-12-28 21:30:15 -0600

    update icons

--- src/utils/IconsCollections.tsx
+++ src/utils/IconsCollections.tsx
@@ -1,56 +1,28 @@
-import {TiSocialTwitter, TiSocialLinkedin, TiSocialFacebook, TiSocialGithub, TiSocialYoutube} from "react-icons/ti";
-
 import {
-	GiClothes, GiForkKnifeSpoon, GiScooter, GiBabyBottle, GiFruitBowl,
-	GiCat, GiPaperClip, GiSofa, GiLipstick, GiTwirlyFlower,
-	GiGamepad, GiIsland, GiPill, GiLoveInjection, GiRunningShoe
-} from "react-icons/gi";
-
-import {
-	RiWaterFlashFill,
-	RiToolsFill,
-	RiPoliceCarFill,
-	RiHotelLine,
-	RiHospitalLine,
-	RiMoneyCnyCircleLine,
-	RiHeartsFill,
-	RiRunLine,
-	RiBookOpenFill, RiGiftLine,
+	RiWaterFlashFill, RiToolsFill,
+	RiPoliceCarFill, RiHotelLine, RiHospitalLine, RiMoneyCnyCircleLine,
+	RiHeartsFill, RiRunLine, RiBookOpenFill, RiGiftLine, RiCopyleftLine, RiCustomerServiceLine,
+	RiMailCheckFill, RiPencilRuler2Line, RiScissorsFill, RiDatabase2Line, RiClipboardLine, RiAuctionFill, RiCoinsFill,
+	RiGift2Line, RiHandCoinLine, RiMoneyDollarCircleLine, RiShoppingCart2Line, RiTrophyFill, RiLungsLine,
+	RiEBike2Line, RiVirusLine, RiGobletLine, RiPoliceCarLine, RiRestaurantFill, RiTruckLine, RiMovie2Line,RiUser5Line, RiTeamLine,RiGroupLine,RiEmotionHappyLine
 } from "react-icons/ri";
 
-import {
-	MdPayment, MdWifiCalling, MdBuild, MdLocalDrink, MdNoDrinks,
-	MdCommute, MdFastfood, MdEmojiFoodBeverage, MdLocalMovies,
-	MdDinnerDining, MdCardGiftcard, MdMiscellaneousServices, MdHouse,
-	MdOutlineEmail, MdSignalWifiStatusbarConnectedNoInternet2, MdOutlineCameraAlt,
-	MdOutlineFlightTakeoff, MdLibraryMusic, MdSubway,
-	MdCreditCard, MdSportsBasketball, MdSportsFootball, MdFitnessCenter, MdOutlineIcecream,
-	MdOutlineChildFriendly, MdOutlineBrush
-} from "react-icons/md";
 
 import {IconType} from "react-icons";
 import generateRandomNumber from "./IdGenerator";
 import React from "react";
 
-export const ExpenseIcons: IconType[] = [MdWifiCalling, MdBuild, MdLocalDrink,
-	MdCommute, MdFastfood, MdEmojiFoodBeverage, MdLocalMovies, MdNoDrinks,
-	MdDinnerDining, MdCardGiftcard, GiClothes, MdMiscellaneousServices, RiWaterFlashFill,
-	MdHouse, RiToolsFill, MdPayment, RiPoliceCarFill, RiHotelLine, MdOutlineEmail,
-	MdSignalWifiStatusbarConnectedNoInternet2, GiForkKnifeSpoon, GiScooter, GiFruitBowl,
-	RiHospitalLine, MdOutlineCameraAlt, TiSocialTwitter, TiSocialLinkedin, TiSocialFacebook,
-	TiSocialGithub, TiSocialYoutube, GiCat, GiSofa, GiLipstick, MdOutlineFlightTakeoff,
-	RiMoneyCnyCircleLine, GiGamepad, GiIsland, GiPill, MdLibraryMusic, GiLoveInjection, MdSubway,
-	MdCreditCard, MdSportsBasketball, MdSportsFootball, MdFitnessCenter, RiHeartsFill, MdOutlineIcecream,
-	RiRunLine, RiBookOpenFill, GiTwirlyFlower, GiPaperClip, RiGiftLine, GiBabyBottle, MdOutlineChildFriendly, MdOutlineBrush,
-	GiRunningShoe]
+export const ExpenseIcons: IconType[] = [RiWaterFlashFill, RiToolsFill,
+	RiPoliceCarFill, RiHotelLine, RiHospitalLine, RiMoneyCnyCircleLine,
+	RiHeartsFill, RiRunLine, RiBookOpenFill, RiGiftLine, RiCopyleftLine, RiCustomerServiceLine,
+	RiMailCheckFill, RiPencilRuler2Line, RiScissorsFill, RiDatabase2Line, RiClipboardLine, RiAuctionFill, RiCoinsFill,
+	RiGift2Line, RiHandCoinLine, RiMoneyDollarCircleLine, RiShoppingCart2Line, RiTrophyFill, RiLungsLine, RiEBike2Line,
+	RiVirusLine, RiGobletLine, RiPoliceCarLine, RiRestaurantFill, RiTruckLine, RiMovie2Line,RiUser5Line, RiTeamLine,RiGroupLine,RiEmotionHappyLine]
 
 
-export const IncomeIcons: IconType[] = [
-	MdPayment, RiGiftLine, GiClothes, MdMiscellaneousServices, RiWaterFlashFill,
-	MdHouse, RiToolsFill, MdPayment, RiPoliceCarFill, RiHotelLine, MdOutlineEmail,
-	MdSignalWifiStatusbarConnectedNoInternet2, GiForkKnifeSpoon, GiScooter, GiFruitBowl,
-	RiHospitalLine, MdOutlineCameraAlt, TiSocialTwitter, TiSocialLinkedin, TiSocialFacebook,
-	TiSocialGithub, TiSocialYoutube, GiCat, GiSofa, GiLipstick, MdOutlineFlightTakeoff]
+export const IncomeIcons: IconType[] = [RiCoinsFill,
+	RiGift2Line, RiHandCoinLine, RiMoneyDollarCircleLine, RiScissorsFill, RiDatabase2Line, RiClipboardLine, RiAuctionFill, RiShoppingCart2Line, RiGiftLine, RiWaterFlashFill
+	, RiToolsFill, RiPoliceCarFill, RiHotelLine, RiPoliceCarLine]
 
 
 export const IncomeIconsHashMap: HashMapType = IncomeIcons.reduce((result: {}, Icon: IconType) => {
