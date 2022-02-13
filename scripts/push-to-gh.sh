e36dfdc50b3603ef80f342e72a558a62976b98e5
GraceYu <librayuyueran@gmail.com>
2022-02-12 22:46:46 -0600

    add the scripts to auto deploy to netlify

---
+++ scripts/push-to-gh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m 'deploy gy spending app to netlify' &&
+git remote add origin git@github.com:Yueran-Yu/spending-app-pro.git &&
+git push -u origin master -f &&
+cd ..
